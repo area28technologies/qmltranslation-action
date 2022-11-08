4416b4be8adfec59fd61b8ff861a5d508f733fb7
Manav Tidhan <manav.tidhan@area28.io>
2022-11-08 18:25:16 +0800

    ci: handle the error handling

--- dockerhub/entrypoint.sh
+++ dockerhub/entrypoint.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
-
-set -e
+#!/bin/bash
 
 echo "running entrypoint command(s)"
 response=$(sh -c " $INPUT_COMMAND")
+status=$?
 echo "::set-output name=response::$response"
+if [[ $status -eq 0 ]]; then exit 0; else exit 1; fi
 
